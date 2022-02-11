1246c7cfe4edb81c574beae15b22fd4ce4df7a3c
DanyloSydorchuk <79981677+DanyloSydorchuk@users.noreply.github.com>
2022-02-11 10:59:51 +0100

    Add project files.

---
+++ 13309zadanie1/Controllers/PlytyController.cs
@@ -0,0 +1,42 @@
+﻿using _13309zadanie1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13309zadanie1.Controllers
+{
+    public class PlytyController : Controller
+    {
+        public static List<Plyty> list = new List<Plyty>() { };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View("Add");
+        }
+        [HttpPost]
+        public ActionResult Add(Plyty plyty)
+        {
+            int index = 0;
+            if (ModelState.IsValid)
+            {
+                plyty.Id = index++;
+                list.Add(plyty);
+                return View("ConfirmAdd", plyty);
+
+            }
+            else
+            {
+                return View("Add");
+            }
+        }
+        public ActionResult FindAll()
+        {
+            return View("List", list);
+        }
+    }
+}
---
+++ 13309zadanie1/Models/Plyty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13309zadanie1.Models
+{
+    public class Plyty
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Proszę podać szerokość płyty")]
+        [Range(0, 2100, ErrorMessage = "Szerokość płyty od 0 do 2100")]
+        public int Szerokosc { get; set; }
+        [Required(ErrorMessage = "Proszę podać wysokość płyty")]
+        [Range(0, 2850, ErrorMessage = "Wysokość płyty od 0 do 2850")]
+        public int Wysokosc { get; set; }
+        [RegularExpression(@"\d+(?:,\d{1,2})?", ErrorMessage = "Prosze podac liczbe z max 2 miejscami po przecinku")]
+        [Required(ErrorMessage = "Proszę podać cenę za 1m^2")]
+        [Range(0, 99999.99, ErrorMessage = "Cena od 0 do 99999,99")]
+        public decimal Cena { get; set; }
+        [Required(ErrorMessage = "Proszę podać ilość płyt")]
+        [Range(0, 200, ErrorMessage = "Ilość płyt od 0 do 200")]
+        public int Ilosc { get; set; }
+    }
+}
